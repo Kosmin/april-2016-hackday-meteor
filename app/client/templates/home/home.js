f6bc1d7f74bc89d0bdcf351bb1c4eaa1d24ea5ff
KenRiku <fresh.ham.man@gmail.com>
2016-04-15 15:59:57 -0700

    commit

--- app/client/templates/home/home.js
+++ app/client/templates/home/home.js
@@ -11,7 +11,7 @@ Template.Home.topGenresChart = function() {
         },
 
         title: {
-            text: 'Risks Heatmap'
+            text: ''
         },
 
         xxxsubtitle: {
@@ -90,25 +90,72 @@ Template.Home.topGenresChart = function() {
             }
         },
 
-        series: [{
-            data: [
-                { x: 95, y: 95, z: 13.8, name: '1', country: 'Geopolitical' },
-                { x: 86.5, y: 82.9, z: 14.7, name: '2', country: 'Tax Compliance' },
-                { x: 80.8, y: 91.5, z: 15.8, name: '3', country: 'Indemnity Risk' },
-                { x: 80.4, y: 102.5, z: 12, name: '4', country: 'Environmental' },
-                { x: 80.3, y: 86.1, z: 11.8, name: '5', country: 'Conduct' },
-                { x: 78.4, y: 70.1, z: 16.6, name: '6', country: 'Audit Strategy Risk' },
-                { x: 74.2, y: 68.5, z: 14.5, name: '7', country: 'Fraud Risk' },
-                { x: 73.5, y: 83.1, z: 10, name: '8', country: 'Geopolitical' },
-                { x: 71, y: 93.2, z: 24.7, name: '9', country: 'Talent Loss' },
-                { x: 69.2, y: 57.6, z: 10.4, name: '10', country: 'OPEX Spending' },
-                { x: 68.6, y: 20, z: 16, name: '11', country: 'Currency Risk' },
-                { x: 65.5, y: 72.4, z: 35.3, name: '12', country: 'Business Continuity' },
-                { x: 65.4, y: 50.8, z: 28.5, name: '13', country: 'Litigation Risks' },
-                { x: 63.4, y: 51.8, z: 15.4, name: '14', country: 'Competitive Pressure' },
-                { x: 64, y: 82.9, z: 31.3, name: '15', country: 'Legal Compliance' }
-            ]
-        }]
+        series: [
+            {
+                data: [{ x: 65.5, y: 72.4, z: 35.3, name: '12', country: 'Business Continuity' }],
+                color: "#DE4D33"
+            },
+            {
+                data: [{ x: 68.6, y: 20, z: 16, name: '11', country: 'Currency Risk' }],
+                color: "#EABA51"
+            },
+            {
+                data: [{ x: 69.2, y: 57.6, z: 10.4, name: '10', country: 'OPEX Spending' }],
+                color: "#DE4D33"
+            },
+            {
+                data: [{ x: 71, y: 93.2, z: 24.7, name: '9', country: 'Talent Loss' }],
+                color: "#DE4D33"
+            },
+            {
+                data: [{ x: 73.5, y: 83.1, z: 10, name: '8', country: 'Geopolitical' }],
+                color: "#DE4D33"
+            },
+            {
+                data: [{ x: 74.2, y: 68.5, z: 14.5, name: '7', country: 'Fraud Risk' }],
+                color: "#DE4D33"
+            },
+            {
+                data: [{ x: 78.4, y: 70.1, z: 16.6, name: '6', country: 'Audit Strategy Risk' }],
+                color: "#DE4D33"
+            },
+            {
+                data: [{ x: 80.3, y: 86.1, z: 11.8, name: '5', country: 'Conduct' }],
+                color: "#DE4D33"
+            },
+            {
+                data: [{ x: 80.4, y: 102.5, z: 12, name: '4', country: 'Environmental' }],
+                color: "#DE4D33"
+            },
+            {
+                data: [{ x: 80.8, y: 91.5, z: 15.8, name: '3', country: 'Indemnity Risk' }],
+                color: "#DE4D33"
+            },
+            {
+                data: [{ x: 86.5, y: 82.9, z: 14.7, name: '2', country: 'Tax Compliance' }],
+                color: "#DE4D33"
+            },
+            {
+                data: [{ x: 45, y: 95, z: 13.8, name: '1', country: 'Geopolitical' }],
+                color: "#EABA51"
+            },
+            {
+                data: [{ x: 35, y: 72.4, z: 35.3, name: '12', country: 'Business Continuity' }],
+                color: "#42996D"
+            },
+            {
+                data: [{ x: 13, y: 50.8, z: 28.5, name: '13', country: 'Litigation Risks' }],
+                color: "#42996D"
+            },
+            {
+                data: [{ x: 20, y: 82.9, z: 31.3, name: '15', country: 'Legal Compliance' }],
+                color: "#EABA51"
+            },
+            {
+                data: [{ x: 3, y: 51.8, z: 15.4, name: '14', country: 'Competitive Pressure' }],
+                color: "#42996D"
+            }
+        ]
     };
 };
 
